63a4bf8600d8bb5eea385cd94006313286b29627
Jon Lebensold <jon@lebensold.ca>
2012-04-14 00:10:27 +0300

    got faceting working

--- public/js/Adapter.js
+++ public/js/Adapter.js
@@ -4,10 +4,12 @@ window.Adapter = Backbone.Model.extend({
     this.fields = options.fields;
     this.data = options.data;
     this.translated = [];
+		this.faceted = {};
   },
 
   transform: function(){
-
+		if(this.translated.length > 0)
+			return this.translated;
     _.each(this.data, function(item) {
       var d = {}
       _.each(this.fields, function(field) {
@@ -19,7 +21,42 @@ window.Adapter = Backbone.Model.extend({
     return this.translated;
   },
 
-  fasets: function(){
-    
-  }
+  facets: function(){
+  	if (this.faceted.length > 0)
+			return this.faceted;
+		_.each(this.transform(),function(item) {
+			_.each(this.fields,function(field) {
+				if (this.faceted[field[0]] == undefined) 
+				{
+					this.faceted[field[0]] = [];
+				}
+				this.faceted[field[0]].push(item[field[0]]);
+			},this);
+		},this);
+		this.faceted = _.map(this.faceted,this.convertField,this);
+		
+		return this.faceted;
+  },
+
+	convertField: function(values,fieldName) {
+		var type = this.fieldType(fieldName);
+		switch(type)
+		{
+			case 'string':
+			case 'collection':
+				return {name: fieldName, values: _.compact(_.uniq(_.flatten(values))), type:type};
+			case 'numberrange':
+				var nums = _.map(values,function(v) { return parseFloat(v); });
+				return {name: fieldName, values: [_.min(nums), _.max(nums)],type:type};
+			case 'daterange':
+				var nums = _.map(values,function(v) { return Date.parse(v); });			
+				return {name: fieldName, values: [new Date(_.min(nums)), new Date(_.max(nums))], type:type};
+		}
+		return {name: fieldName,values: values, type:type};
+	},
+
+	fieldType: function(field) {
+		var f = _.find(this.fields,function(f) { return f[0] == field; });
+		return f[2];
+	}
 });
--- spec/javascripts/AdapterSpec.js
+++ spec/javascripts/AdapterSpec.js
@@ -5,21 +5,31 @@ describe("Adapter", function() {
   beforeEach(function() {
     data = fixtures.sample_response.response.results;
     fields = [
-      ['thumbnail', function(item) {return item.fields.thumbnail;} ],
-      ['shortUrl', function(item) { return item.fields.shortUrl;} ],
-      ['score', function(item) { return item.fields.score; }],
-      ['webPublicationDate', 'webPublicationDate'],
-      ['webTitle','webTitle'],
-      ['tags',function(item) { return _.map(item.tags,function(t) { return t.sectionName;})}]
+      ['thumbnail', function(item) {return item.fields.thumbnail;}, 'string'],
+      ['shortUrl', function(item) { return item.fields.shortUrl;} ,'string'],
+      ['score', function(item) { return item.fields.score; }, 'numberrange'],
+      ['webPublicationDate', 'webPublicationDate','daterange'],
+      ['webTitle','webTitle','string'],
+      ['tags',function(item) { return _.compact(_.uniq(_.map(item.tags,function(t) { return t.sectionName;})))},'collection']
     ];
   });
 
-  it('should be awesome', function() {
+  it('can parse data with fields', function() {
 
     var adapter = new Adapter({fields:fields, data:data});
     var translated = adapter.transform();
-
-
     expect(translated[0].thumbnail).toEqual("http://static.guim.co.uk/sys-images/Media/Columnists/Columnists/2011/10/4/1317734975988/Apple-store-in-Beijing-003.jpg");
   });
+
+	it('can determine facets from data',function() {
+    var adapter = new Adapter({fields:fields, data:data});
+		var faceted = adapter.facets();
+		expect(faceted[0].type).toEqual('string');
+		expect(faceted[1].type).toEqual('string');
+		expect(faceted[2].type).toEqual('numberrange');
+		expect(faceted[3].type).toEqual('daterange');
+		expect(faceted[3].values[0].toString()).toEqual('Sat Apr 07 2012 02:05:03 GMT+0300 (IDT)');
+		expect(faceted[3].values[0].getDay()).toEqual(6);
+		expect(faceted[5].values.length).toEqual(28);
+	});
 });
